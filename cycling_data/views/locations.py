dec0607c5770984ab76a2c484d2f8b0a7b2837e7
John Haiducek <jhaiduce@gmail.com>
2019-02-16 19:33:43 -0500

    More targeted search query for location autocomplete

--- cycling_data/views/locations.py
+++ cycling_data/views/locations.py
@@ -2,7 +2,7 @@ from pyramid.view import view_config
 import colander
 import deform.widget
 
-from ..models.cycling_models import Location
+from ..models.cycling_models import Location, Ride
 
 class LocationViews(object):
     def __init__(self, request):
@@ -11,7 +11,20 @@ class LocationViews(object):
     @view_config(route_name='locations_autocomplete', renderer='json')
     def ride_table(self):
         term=self.request.GET['term']
+
+        # Subquery to get the first ride of each location
+        last_id = self.request.dbsession.query(Ride.id).filter(
+            Ride.startloc_id==Location.id
+                or Ride.endloc_id==Location.id
+        ).order_by(Ride.start_time.desc()).limit(1).correlate(Location)
+
+        # Query locations matching the term variable,
+        # most recently used first
         locations=self.request.dbsession.query(Location).filter(
-            Location.name.startswith(term))
+                Location.name.startswith(term)
+            ).outerjoin(Ride, Ride.id == last_id
+            ).order_by(Ride.start_time.desc()
+            ).order_by(Location.id.desc()).limit(8)
+
         return [location.name for location in locations]
 
