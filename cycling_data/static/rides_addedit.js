52f8fa68cd36ebc32525368a7b77a49844e177a7
John Haiducek <jhaiduce@picard.local>
2019-03-21 22:08:35 -0400

    Function to check that the average speed is consistent with the entered time and distance

--- cycling_data/static/rides_addedit.js
+++ cycling_data/static/rides_addedit.js
@@ -1,4 +1,25 @@
 $(function() {
+
+    jQuery.validator.addMethod('avspeed_consistent',function(value,element){
+	try{
+	    distance=parseFloat($("input[name='distance']").val())
+	}
+	catch(TypeError){
+	    return true;
+	}
+	try{
+	    t=$("input[name='rolling_time']").val().split(':').map(parseFloat)
+	}
+	catch(TypeError){
+	    return true;
+	}
+	hours=t[0]+t[1]/60.0+t[2]/3600.0
+	calcspeed=distance/hours
+	amountOff=(value/calcspeed-1)
+	if(Math.abs(amountOff)>0.1) return false;
+	else return true;
+    },'Average speed may not be consistent with the time and distance entered');
+    
   // Initialize form validation on the registration form.
   // It has the name attribute "registration"
     $("form[id='deform']").validate({
@@ -17,7 +38,8 @@ $(function() {
 	    },
 	    avspeed:{
 		min:0,
-		number:true
+		number:true,
+		avspeed_consistent:true
 	    }
 	},
     });
