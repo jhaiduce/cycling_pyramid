ea364c31b7bf3d32f4fdaa922dec5dee3dceef8f
John Haiducek <jhaiduce@picard.local>
2020-01-06 16:09:59 -0500

    Updated deployment script to work with new Docker images

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-docker build -t jhaiduce/cycling-pyramid -f Dockerfile . && \
-    docker push jhaiduce/cycling-pyramid && \
+docker build -t jhaiduce/cycling-pyramid-base -f Dockerfile.base . && \
+    docker build -t jhaiduce/cycling-pyramid-web -f Dockerfile.web . && \
+    docker build -t jhaiduce/cycling-pyramid-worker -f Dockerfile.worker . && \
+    docker push jhaiduce/cycling-pyramid-base && \
+    docker push jhaiduce/cycling-pyramid-worker && \
+    docker push jhaiduce/cycling-pyramid-web && \
     docker stack deploy -c docker-compose.yml cycling_stack && \
-    docker service update --image jhaiduce/cycling-pyramid cycling_stack_cycling_web
+    docker service update --image jhaiduce/cycling-pyramid-web cycling_stack_cycling_web && \
+    docker service update --image jhaiduce/cycling-pyramid-worker cycling_stack_worker
