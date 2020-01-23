ccb9dffd2df15d34cc428bf5860d85531527df98
John Haiducek <jhaiduce@picard.local>
2020-01-22 23:41:24 -0500

    Added a test of adding a ride

--- integration_tests.py
+++ integration_tests.py
@@ -33,6 +33,36 @@ class BaseTest(unittest.TestCase):
         resp=self.session.get('http://cycling_test_cycling_web/rides')
         self.assertGreater(resp.text.find('total rides'),0)
 
+    def test_ride_add(self):
+        import re
+        resp=self.session.get('http://cycling_test_cycling_web/rides')
+        ride_count=int(re.search(r'<p>(\d+) total rides</p>',resp.text).group(1))
+
+        resp=self.session.post(
+            'http://cycling_test_cycling_web/rides/add',
+            data=dict(
+                start_time='2005-01-01 10:00:00',
+                end_time='2005-01-01 10:15:00',
+                total_time='00:15:00',
+                rolling_time='00:12:00',
+                distance='7',
+                odometer='357',
+                avspeed='28',
+                maxspeed='40',
+                equipment_id='0',
+                ridergroup_id='0',
+                surface_id='0',
+                submit='submit',
+                startloc='Home',
+                endloc='Work'
+            )
+        )
+
+        resp=self.session.get('http://cycling_test_cycling_web/rides')
+        ride_count_after=int(re.search(r'(\d+) total rides',resp.text).group(1))
+
+        self.assertEqual(ride_count_after,ride_count+1)
+
     def tearDown(self):
         resp=self.session.post('http://cycling_test_cycling_web/logout')
         self.assertEqual(resp.history[0].status_code,302)
