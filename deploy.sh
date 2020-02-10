f5333c5cebf33bd5ba1c1b146c77ce03d7f4730d
John Haiducek <jhaiduce@picard.local>
2020-02-10 18:38:22 -0500

    Updated container names in deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker-compose build && \
     docker-compose push && \
     docker stack deploy -c docker-compose.yml cycling_stack && \
-    docker service update --image jhaiduce/cycling-pyramid-web cycling_stack_cycling_web && \
-    docker service update --image jhaiduce/cycling-pyramid-worker cycling_stack_worker
+    docker service update --image jhaiduce/cycling-pyramid cycling_stack_cycling_web && \
+    docker service update --image jhaiduce/cycling-pyramid cycling_stack_worker
