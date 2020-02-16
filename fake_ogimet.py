d10280f11226d3954db21284fa587e9bc291bef1
John Haiducek <jhaiduce@picard.local>
2020-02-16 12:04:55 -0500

    Added a JSON cache file for OGIMET queries

--- fake_ogimet.py
+++ fake_ogimet.py
@@ -1,16 +1,47 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+import os
+import json
+import logging
+log = logging.getLogger(__name__)
 
-def ogimet_request(request):
-    import requests
-    resp=requests.get('https://ogimet.com/display_metars2.php',params=request.params)
-    return Response(resp.text)
+class ogimet_proxy(object):
+
+    cache_file='/app/ogimet-cache/ogimet-cache.json'
+
+    def __init__(self):
+        self.cache={}
+        if os.path.isfile('ogimet-cache.json'):
+            with open(self.cache_file,'r') as fh:
+                stored_cache=json.load(fh)
+                
+                for item in stored_cache:
+                    self.cache[frozenset(item['params'].items())]=item.text
+
+    def write_cache(self):
+        with open(self.cache_file,'w') as fh:
+            stored_cache=[{'params':dict(params),'text':text}
+                          for params,text in self.cache.items()]
+            json.dump(stored_cache,fh,indent=4)
+    
+    def ogimet_request(self,request):
+        import requests
+
+        try:
+            text=self.cache[frozenset(request.params.items())]
+        except KeyError:
+            text=requests.get('https://ogimet.com/display_metars2.php',params=request.params).text
+            if 'METAR' in text:
+                self.cache[frozenset(request.params.items())]=text
+                self.write_cache()
+        return Response(text)
 
 if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('ogimet_request', '/')
-        config.add_view(ogimet_request, route_name='ogimet_request')
+        proxy=ogimet_proxy()
+        config.add_view(proxy.ogimet_request, route_name='ogimet_request')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 80, app)
     server.serve_forever()
