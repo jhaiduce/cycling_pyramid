ed0da58613f9b9e067ab79dc47fe0d771d3d2e1c
John Haiducek <jhaiduce@picard.local>
2020-02-16 16:43:21 -0500

    Exit script if commands exit with an error

--- pyramid_start.sh
+++ pyramid_start.sh
@@ -2,4 +2,12 @@
 
 /usr/bin/initialize_cycling_data_db /run/secrets/production.ini
 
+if [ $? -ne 0 ]; then
+    exit $?;
+fi
+
 /usr/bin/pserve /run/secrets/production.ini
+
+if [ $? -ne 0 ]; then
+    exit $?;
+fi
