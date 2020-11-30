7d20fdbb58eb66820c64ddbca3b925383f4acd87
John Haiducek <jhaiduce@gmail.com>
2020-11-29 20:04:39 -0500

    Fix selinux labels before running tests

--- test.sh
+++ test.sh
@@ -4,6 +4,13 @@ sudo docker kill cycling_test_cycling_web
 sudo docker kill cycling_test_worker
 sudo docker kill ci_sut_1
 
+declare -a secret_files=("integration_test.ini" "mysql_root_password_test" "ca.pem")
+
+for file in "${arr[@]}"
+do
+    chcon -t svirt_sandbox_file_t "$file"
+done
+
 $VENV/bin/pytest -q &&\
     sudo docker-compose -f docker-compose.test.yml -p ci build && \
     sudo docker-compose -f docker-compose.test.yml -p ci up -d && \
