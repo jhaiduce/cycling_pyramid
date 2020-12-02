b4a1fd6e8e5798369b0a71e3fe972d7fbf6b23aa
John Haiducek <jhaiduce@gmail.com>
2020-12-02 13:19:05 -0500

    Run database migration as a seprate service rather than as part of the entrypoint of the web server process

--- test.sh
+++ test.sh
@@ -12,10 +12,18 @@ $VENV/bin/pytest -q
 $VENV/bin/pytest -q cycling_data/migration_tests.py
 
 # Build images
-sudo docker-compose -f docker-compose.test.yml -p ci build
+sudo docker-compose -f docker-compose.test_secrets.yml -f docker-compose.db.yml -f docker-compose.test.yml -p ci build
+
+# Migrate database
+sudo docker-compose -f docker-compose.test_secrets.yml -f docker-compose.db.yml -f docker-compose.migrate.yml -p ci up -d
+
+sudo docker wait ci_migration_1
+
+# Print migration logs
+sudo docker logs ci_migration_1
 
 # Run tests
-sudo docker-compose -f docker-compose.test.yml -p ci up -d
+sudo docker-compose -f docker-compose.test_secrets.yml -f docker-compose.db.yml -f docker-compose.test.yml -p ci up -d
 
 # Print test logs
 sudo docker logs -f ci_sut_1
