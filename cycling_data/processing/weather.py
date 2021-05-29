7f77a76a2f0d7842fde2ead0cfbf3260b1e376b8
John Haiducek <jhaiduce@gmail.com>
2021-05-28 23:22:14 -0400

    Removed commented-out code

--- cycling_data/processing/weather.py
+++ cycling_data/processing/weather.py
@@ -98,11 +98,9 @@ def download_metars(station,dtstart,dtend,dbsession=None,task=None):
     except:
         ogimet_url='https://www.ogimet.com/display_metars2.php'
 
-    #past_requests=dbsession.query(SentRequestLog).order_by(SentRequestLog.time.desc()).limit(100)
     last_request_time=dbsession.query(func.max(SentRequestLog.time).label('time')).one().time
     requests_last_hour=dbsession.query(SentRequestLog).filter(
         SentRequestLog.time>datetime.now()-timedelta(seconds=3600*60)).count()
-
     min_delay_seconds=1
     random_delay_scale=1
     retry_delay=random_delay(min_delay_seconds,random_delay_scale)
