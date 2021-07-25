eecb90b87473fa79206d7d729e45d433cde613d2
John Haiducek <jhaiduce@gmail.com>
2021-07-24 21:03:17 -0400

    Don't fail test if the task raises a rate limiting exception

--- integration_tests.py
+++ integration_tests.py
@@ -193,7 +193,13 @@ class BaseTest(unittest.TestCase):
         # Wait for update_ride_weather task to complete
         from celery.result import AsyncResult
         task_result=AsyncResult(update_weather_task_id,app=celery)
-        task_result.wait(60)
+        try:
+            task_result.wait(60)
+        except RuntimeError as e:
+            if str(e).startswith('Last OGIMET request was'):
+                pass
+            else:
+                raise
 
         # Check the weather data in the ride details page
         resp=self.session.get('http://cycling_test_cycling_web/rides/{:d}/details'.format(ride_id))
